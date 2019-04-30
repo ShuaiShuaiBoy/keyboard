4337f8a33282155de5d2f319a3a1a2a213d2b607
ShuaiShuai <cjs19960625@outlook.com>
2019-04-30 10:42:39 +0800

    基本框架

---
+++ src/test/java/cn/com/codepower/keyboard/KeyboardApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.com.codepower.keyboard;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KeyboardApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
